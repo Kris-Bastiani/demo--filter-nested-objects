f9e1d73da9d4a59bc522a194251ddd383b71f49e
Kris Bastiani <kris.bastiani@gmail.com>
2020-06-01 17:02:14 +1000

    save petsByOwnerGender in mocks

---
+++ __mocks__/petsByOwnerGender.js
@@ -0,0 +1,16 @@
+module.exports = {
+	male: [
+		{ name: 'Garfield', type: 'Cat' },
+		{ name: 'Fido', type: 'Dog' },
+		{ name: 'Tom', type: 'Cat' },
+		{ name: 'Max', type: 'Cat' },
+		{ name: 'Sam', type: 'Dog' },
+		{ name: 'Jim', type: 'Cat' },
+	],
+	female: [
+		{ name: 'Garfield', type: 'Cat' },
+		{ name: 'Tabby', type: 'Cat' },
+		{ name: 'Simba', type: 'Cat' },
+		{ name: 'Nemo', type: 'Fish' },
+	],
+};
--- __tests__/getPetsByOwnerGender.test.js
+++ __tests__/getPetsByOwnerGender.test.js
@@ -1,25 +1,9 @@
 const mockData = require('../__mocks__/people.json');
+const petsByOwnerGender = require('../__mocks__/petsByOwnerGender');
 const getPetsByOwnerGender = require('../lib/helpers/getPetsByOwnerGender');
-
-const EXPECTED_RESULT = {
-	male: [
-		{ name: 'Garfield', type: 'Cat' },
-		{ name: 'Fido', type: 'Dog' },
-		{ name: 'Tom', type: 'Cat' },
-		{ name: 'Max', type: 'Cat' },
-		{ name: 'Sam', type: 'Dog' },
-		{ name: 'Jim', type: 'Cat' },
-	],
-	female: [
-		{ name: 'Garfield', type: 'Cat' },
-		{ name: 'Tabby', type: 'Cat' },
-		{ name: 'Simba', type: 'Cat' },
-		{ name: 'Nemo', type: 'Fish' },
-	],
-};
 
 describe('getPetsByOwnerGender()', () => {
 	it('Returns the expected object', () => {
-		expect(getPetsByOwnerGender(mockData)).toMatchObject(EXPECTED_RESULT);
+		expect(getPetsByOwnerGender(mockData)).toMatchObject(petsByOwnerGender);
 	});
 });
